0e9dea7e383e0c37322d85fba1281be57a740944
draffensperger <daveraff@google.com>
2017-10-18 09:21:50 -0400

    Fixes and more config

--- app.js
+++ app.js
@@ -22,38 +22,26 @@ var bunyanLogger = bunyan.createLogger(
 var metadata = {resource: {type: 'global'}};
 
 app.get('/', function(req, res) {
-  if (logWinston) {
-    winston.info('See Winston ...', null);
-  }
-  if (logBunyan) {
-    bunyanLogger.info('See Bunyan ...');
-  }
+  winston.info('See Winston ...', null);
+  bunyanLogger.info('See Bunyan ...');
 
   var childSpan = traceApi.createChildSpan({name: 'Custom span'});
 
   setTimeout(function() {
-    if (logWinston) {
-      winston.info('... log entries ...', {extra: 'info'});
-      winston.info(
-          'Winston custom trace ID', {[transport.LOGGING_TRACE_KEY]: 'trace1'});
-    }
-    if (logBunyan) {
-      bunyanLogger.info('... log entries ...');
-      bunyanLogger.info(
-          {[LoggingBunyan.LOGGING_TRACE_KEY]: 'trace2'},
-          'Bunyan custom trace ID');
-      bunyanLogger.info('Superduperlonglogentry'.repeat(20));
-    }
+    winston.info('... log entries ...', {extra: 'info'});
+    winston.info(
+        'Winston custom trace ID', {[transport.LOGGING_TRACE_KEY]: 'trace1'});
+    bunyanLogger.info('... log entries ...');
+    bunyanLogger.info(
+        {[LoggingBunyan.LOGGING_TRACE_KEY]: 'trace2'},
+        'Bunyan custom trace ID');
+    bunyanLogger.info('Superduperlonglogentry'.repeat(20));
     childSpan.endSpan();
   }, 5);
 
   setTimeout(function() {
-    if (logWinston) {
-      winston.error('... among trace spans!', null);
-    }
-    if (logBunyan) {
-      bunyanLogger.error('... among trace spans!');
-    }
+    winston.error('... among trace spans!', null);
+    bunyanLogger.error('... among trace spans!');
     res.end('Hello world\n');
   }, (10.0 + Math.random() * 50.0));
 })
